cd49ee1f68ab8efe9f3e5543cc9bddcbf73185d5
Tobias Sutter <64151843+tobsutter@users.noreply.github.com>
2021-10-06 15:02:08 +0200

    Update main_OPE.m

--- OPE/main_OPE.m
+++ OPE/main_OPE.m
@@ -33,13 +33,16 @@ param.beta = 4; %propensitiy regularization parameter
 
 
 
-T_vec = linspace(10,400,5);
+T_vec = linspace(10,400,20);
+T_vec = linspace(10,1000,20);
 
 
 
 param.r = 10^(-2);    %DRO radius
 
-N_ex = 5; % #experiments
+N_ex = 50; % #experiments
+N_ex = 50;
+
 
 w_noise = random('geo',param.t,1,10^6);
 
@@ -66,11 +69,13 @@ T_eval = 10^5;
 x_last=0; %initial condition
 x_traj = [];
 a_traj = [];
-occ = zeros(n,m);
+occ_e_check = zeros(n,m);
+occ_e = zeros(n,m);
 cost_traj = [];
 for tt = 1:T_eval
     a_distr = Pi_e(x_last+1,:); %select randomized policy
     a = gendist(a_distr,1,1)-1; %draw policy realization
+   % a = discretesample(a_distr,1)-1; %draw policy realization
     a_traj = [a_traj; a];
     w = w_noise(tt); %draw random demand    
     x_next = max(0,min(n-1,x_last+ a)-w);
@@ -78,21 +83,97 @@ for tt = 1:T_eval
     cost_traj = [cost_traj; cost(x_next,a,param)];
     x_last = x_next;
     
-    %compute occupation measure
+        %compute occupation measure
     for i=1:n
         for j=1:m
             if x_next == x_grid(i) && a == a_grid(j)
-                occ(i,j) = occ(i,j)+1;
+                occ_e_check(i,j) = occ_e_check(i,j)+1;
             end
         end
     end
- 
+    
+    
 end
-occ_e = 1/T_eval.*occ; %empirical behavioural occupation measure 
+
+%ground truth
+V_e_true = sum(cost_traj)/T_eval
+
+
+%compute stationary distribution
+for in=1:n
+    prob_x_e(in) = length(find(x_traj==in-1))/T_eval;
+end
+
+    %compute occupation measure
+    for i=1:n
+        for j=1:m
+                occ_e(i,j) = Pi_e(i,j)*prob_x_e(i);
+        end
+    end
 
 occ_e = occ_e + ones(size(occ_e)).*10^(-15); %avoid singularities
 
+occ_e_check = 1/T_eval.*occ_e_check; %empirical behavioural occupation measure 
+
+
+
+
+
+
+
+%% compute true (unknown) behavioural frequency
+T_eval = 10^5;
+
+%simulate occupation measure
+x_last=0; %initial condition
+x_traj = [];
+a_traj = [];
+cost_traj = [];
+for tt = 1:T_eval
+    a_distr = Pi_b(x_last+1,:); %select randomized policy
+    a = gendist(a_distr,1,1)-1; %draw policy realization
+   % a = discretesample(a_distr,1)-1; %draw policy realization
+    a_traj = [a_traj; a];
+    w = w_noise(tt); %draw random demand    
+    x_next = max(0,min(n-1,x_last+ a)-w);
+    x_traj = [x_traj;x_next];
+    cost_traj = [cost_traj; cost(x_next,a,param)];
+    x_last = x_next; 
+    
+end
+
+
+
+%compute stationary distribution
+for in=1:n
+    prob_x_b_true(in) = length(find(x_traj==in-1))/T_eval;
+end
+
+
+
+
+
+
+
+
+
+
 %% True (unknown) evaluation cost
+
+
+%correction term for pi_e
+Pi_e_c = zeros(n,m);
+for i=1:n
+    for j=1:m
+        Pi_e_c(i,j) = occ_e_check(i,j)/prob_x_e(i);
+    end
+end
+
+% use this corrected evaluation policy from now on ....
+
+
+
+for ij = 1:10
 
 c_eval = 0;
 
@@ -100,15 +181,29 @@ x = 0; %initial condition
 a = 0;
 
 for tt=1:T_eval
-    a_distr = Pi_e(x+1,:); %select randomized policy
+    a_distr = Pi_e_c(x+1,:); %select randomized policy
     a = gendist(a_distr,1,1)-1; %draw policy realization
+ %  a = discretesample(a_distr,1)-1; %draw policy realization
     w = w_noise(tt); %draw random demand
     x = max(0,min(n-1,x+ a)-w);
     c_eval = c_eval + cost(x,a,param);
 end
 
 %disp('true (unknown) evaluation cost')
-V_e = c_eval/T_eval;
+V_e(ij) = c_eval/T_eval;
+end
+
+V_e = mean(V_e)
+
+
+
+
+
+
+
+
+
+
 
 
 disap_DRO = zeros(length(T_vec),N_ex);
@@ -131,7 +226,7 @@ for i_ex = 1:N_ex
 %% Behavioural occupation and cost
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%Step 1: find true occupation meausre
+%Step 1: find true occupation measure
 
 
 %behavioual occupupation measure and cost
@@ -145,6 +240,7 @@ cost_traj = [];
 for tt = 1:T_sim
     a_distr = Pi_b(x_last+1,:); %select randomized policy
     a = gendist(a_distr,1,1)-1; %draw policy realization
+   % a = discretesample(a_distr,1)-1; %draw policy realization
     a_ind = find(a==a_grid);
     a_traj = [a_traj; a];
     w = w_noise(tt); %draw random demand
@@ -155,7 +251,7 @@ for tt = 1:T_sim
     x_last = x_next;
     cost_matrix_b(x_ind,a_ind)=cost_matrix_b(x_ind,a_ind)+cost(x_next,a,param);
     
-    %compute occupation measure
+        %compute occupation measure
     for i=1:n
         for j=1:m
             if x_next == x_grid(i) && a == a_grid(j)
@@ -163,19 +259,61 @@ for tt = 1:T_sim
             end
         end
     end
- 
 end
 
-occ = occ + ones(size(occ)).*10^(-15); %avoid singularities
 
-occ_b = 1/T_sim.*occ; %empirical behavioural occupation measure   
+
+%compute stationary distribution
+for in=1:n
+    prob_x_b(in) = length(find(x_traj==in-1))/T_sim;
+end
+
+    %compute occupation measure
+    for i=1:n
+        for j=1:m
+                occ_b(i,j) = Pi_b(i,j)*prob_x_b(i);
+        end
+    end
+    
+
+
+%empirical occupations measure
+occ_b_check = occ + ones(size(occ)).*10^(-8); %avoid singularities
+
+occ_b_check = 1/T_sim.*occ_b_check; %empirical behavioural occupation measure  
 cost_matrix_b = cost_matrix_b./T_sim;
+
+prob_x_b = (prob_x_b + 10^(-8)*ones(1,length(prob_x_b)))/sum(prob_x_b + 10^(-8)*ones(1,length(prob_x_b)));
+
+
+
+%correction term for pi_b
+Pi_b_c = zeros(n,m);
+for i=1:n
+    for j=1:m
+        Pi_b_c(i,j) = occ_b_check(i,j)/prob_x_b(i);
+    end
+end
+
+% use this corrected behavioural policy from now on ....
+
+
+%avoid singularities
+for i=1:n
+    Pi_b_c(i,:) = (Pi_b_c(i,:) + ones(1,m)*10^(-4))/sum(Pi_b_c(i,:) + ones(1,m)*10^(-4));
+end
+
+
+
+Pi_b_c = Pi_b;
+Pi_e_c = Pi_e;
+
 
 
     
 %% Inverse propensity weights
 
-we = occ_e./occ_b;
+we = occ_e_check./occ_b_check;
 %disp('Inverse propensity method')
 V_IPS(t_s, i_ex) = sum(sum(cost_matrix_b.*we));
 
@@ -200,9 +338,9 @@ buf = 0;
 q = zeros(n*m,1);
 for i=1:n
     for j=1:m
-        buf = buf + occ_e(i,j)*log(occ_e(i,j)/occ_b(i,j));
-        a1((i-1)*m+j) = log(occ_e(i,j)/occ_b(i,j));
-        q((i-1)*m+j) = occ_b(i,j);
+        buf = buf + occ_e_check(i,j)*log(occ_e_check(i,j)/occ_b_check(i,j));
+        a1((i-1)*m+j) = log(occ_e_check(i,j)/occ_b_check(i,j));
+        q((i-1)*m+j) = occ_b_check(i,j);
     end
 end
 alpha = buf;
@@ -211,7 +349,7 @@ A1(1,:) = a1;
 b1 = [alpha];
 A2 = A;
 b2 = b;
-mub = occ_b;
+mub = occ_b_check;
 
 A = [A1;A2];
 b = [b1;b2];
@@ -284,7 +422,7 @@ for i=1:n
         x = x_grid(i);
         a = a_grid(j);
         c_vec((i-1)*m+j) = cost(x,a,param);
-        p_e((i-1)*m+j) = occ_e(i,j);
+        p_e((i-1)*m+j) = occ_e_check(i,j);
     end
 end
 
@@ -308,29 +446,300 @@ V_DRO_alternative(t_s, i_ex) = sum(c_vec.*p_star);
 
 
 
+
+
+
+
+
+
+
+
+
+
+%% doubly robust method [Jiang, Li 2016]
+ 
+s_data = x_traj;
+a_data = a_traj;
+
+
+H=T_sim;
+
+
+% 
+% 
+% 
+% 
+% %compute optimal Q (for simplicity we assume this is possible)
+% %run Bellman interations
+%  
+% V(:,1) = zeros(n,1);
+% 
+% 
+% 
+% for h=1:H
+% 
+% for i=1:n
+%     x = x_grid(i);
+%     for j=1:m
+%         a = a_grid(j);
+%         %Monte-Carlo approximation to the integral
+%         N_MC = 5000;
+%         w_n = random('geo',param.t,1,N_MC);
+%         for y=1:length(w_n)
+%             x_next(y) = max(0,min(n-1,x+ a)-w_n(y));
+%         end
+%         for iy =1:n
+%             ind = find(x_next == iy-1);
+%             q_kern(iy)= 1/N_MC*length(ind);
+%         end
+%         buf = 0;
+%         for iy=1:n
+%             buf = buf + V(iy,h)*q_kern(iy);
+%         end
+%         Q(i,j,h) = cost(x,a,param) + buf;
+%     end
+% end
+% 
+% for i=1:n
+%     x = x_grid(i);
+%     buf = 0;
+%     for j=1:m
+%         a=a_grid(j);
+%         %buf = buf+Q(x+1,a+1)*Pi_e(x+1,a+1);
+%         buf = buf+Q(x+1,a+1,h)*Pi_e_c(x+1,a+1);
+%     end
+%     V(i,h+1) = buf;
+% end
+% end
+% 
+% 
+% % test naiive value iteration:
+% disp('test value iteration')
+% V(:,end)/H  
+% 
+% 
+
+
+% SOLVE LP - approach taken by the infinite horizon doubly robust paper
+
+f = zeros(1,n);
+f = [1 f]; %objective
+
+A = -eye(n);
+A = [-ones(n,1) A];
+for i=1:n
+    x = x_grid(i);
+    buf = zeros(n,1);
+    for j=1:m
+        a = a_grid(j);
+        %Monte-Carlo approximation to the integral
+        N_MC = 5000;
+        w_n = random('geo',param.t,1,N_MC);
+        for y=1:length(w_n)
+            x_next(y) = max(0,min(n-1,x+ a)-w_n(y));
+        end
+        for iy =1:n
+            ind = find(x_next == iy-1);
+            q_kern(iy)= 1/N_MC*length(ind);
+        end
+        for ix = 1:n
+            buf(ix) = buf(ix) + Pi_e_c(x+1,a+1)*q_kern(ix);
+        end
+    end
+    for ix=1:n
+        A(i,ix+1) = A(i,ix+1) + buf(ix);
+    end
+end
+   
+%find b
+for i=1:n
+    x = x_grid(i);
+    buf = 0;
+    for j=1:m
+        a = a_grid(j);
+        buf = buf - Pi_e_c(x+1,a+1)*cost(x,a,param);
+    end
+    bb(i) = buf;
+end
+        
+%solve LP
+x = linprog(f,A,bb);
+optimal_value = x(1)
+Value_fct = x(2:end)
+
+
+
+%generate data:multiple behavioral trajectories
+%behavioual occupupation measure and cost
+N_tra = 100;
+x_traj_tot = x_traj;
+a_traj_tot = a_traj;
+w_noise_new = random('geo',param.t,1,10^6);
+
+
+a_save = a_traj;
+
+for i_t = 1:N_tra-1
+
+x_last=0; %initial condition
+
+x_traj = [];
+a_traj = [];
+occ = zeros(n,m);
+cost_traj = [];
+for tt = 1:T_sim
+    a_distr = Pi_b(x_last+1,:); %select randomized policy
+    a = gendist(a_distr,1,1)-1; %draw policy realization
+   % a = discretesample(a_distr,1)-1; %draw policy realization
+    a = a_save(tt);
+    a_ind = find(a==a_grid);
+    a_traj = [a_traj; a];
+    w = w_noise_new(tt); %draw random demand
+    x_next = max(0,min(n-1,x_last+ a)-w);
+    x_ind = find(x_next == x_grid);
+    x_traj = [x_traj;x_next];
+    cost_traj = [cost_traj; cost(x_next,a,param)];
+    x_last = x_next;
+end
+
+x_traj_tot = [x_traj_tot x_traj];
+a_traj_tot = [a_traj_tot a_traj];
+end
+
+
+
+
+
+%continue with infinite horizon doubly robust paper
+for i=1:n
+    x = x_grid(i);
+    for j=1:m
+        a = a_grid(j);
+        beta(i,j) = Pi_e_c(i,j)/Pi_b(i,j);
+      % beta(i,j) = Pi_e(i,j)/Pi_b(i,j);
+    end
+    w_DR(i) = prob_x_e(i)/prob_x_b_true(i);
+end
+
+
+nom = 0;
+denom = 0;
+for i_t = 1:N_tra
+    s_data = x_traj_tot(:,i_t);
+    a_data = a_traj_tot(:,i_t);
+    for t=1:H-1
+        x = s_data(t);
+        a = a_data(t);
+        nom = nom + w_DR(x+1)*(beta(x+1,a+1)*(cost(x,a,param) + Value_fct(s_data(t+1)+1))-Value_fct(x+1));
+        denom = denom + w_DR(x+1);
+    end
+end
+
+
+R_DR(t_s, i_ex) = nom/denom;
+
+
+
+
+
+% 
+% % as a parity check - run classical DP (1. Value iteration, then Corollary
+% % 5.6.6 from hernandez lerma ===> this is optimal control (different)
+% 
+% V_HL(:,1) = zeros(n,1);     %initialization
+% for it = 1:H    %run value iteration
+%     for ix = 1:n
+%         x = ix-1;
+%         buf_min =10^6;
+%         for ia = 1:m
+%             a = ia-1;
+%              %Monte-Carlo approximation to the integral
+%              N_MC = 5000;
+%              w_n = random('geo',param.t,1,N_MC);
+%             for y=1:length(w_n)
+%                 x_next(y) = max(0,min(n-1,x+ a)-w_n(y));
+%             end
+%             for iy =1:n
+%                 ind = find(x_next == iy-1);
+%                 q_kern(iy)= 1/N_MC*length(ind);
+%             end
+%             buf = 0;
+%             for iy=1:n
+%                 buf = buf + V_HL(iy,it)*q_kern(iy);
+%             end
+%             %find minimum in bellman
+%             V_buf = cost(x,a,param) + buf;
+%             if V_buf < buf_min
+%                 buf_min = V_buf;
+%                 V_HL(ix,it+1) = V_buf;
+%             end
+%         end
+%     end
+% end
+%         
+%  V_HL_final = V_HL(:,end)/H       
+%   
+
+
+
+
+
+% for tt=1:H
+%     x_ind = s_data(tt)+1;
+%     a_ind = a_data(tt)+1;
+%     
+%     disp('r_t + V_t - Q(s_t,a_t)')
+%     cost_traj(tt) + V_DR(tt)-Q(x_ind,a_ind,tt)
+%     
+%     V_DR(tt+1) = V_hat(x_ind) + rho(tt)*(cost_traj(tt) + V_DR(tt)-Q(x_ind,a_ind,tt));
+%    %V_DR(tt+1) = V_hat(x_ind) + rho(tt)*(cost_traj(tt) + V_DR(tt)-Q(x_ind,a_ind) - V_b);
+%     
+%     buf_fin = buf_fin + V_DR(tt+1);
+%     
+% end
+
+
+
+
+
 %compute disappointments
 %===========================
 
 %IPS
-if V_e >= V_IPS(t_s, i_ex)
+if V_e_true >= V_IPS(t_s, i_ex)
     disap_IPS(t_s, i_ex) = 1;
 else
     disap_IPS(t_s, i_ex) = 0;
 end
 
 %Beta-IPS
-if V_e >= V_IPS_beta(t_s, i_ex)
+if V_e_true >= V_IPS_beta(t_s, i_ex)
     disap_IPS_beta(t_s, i_ex) = 1;
 else
     disap_IPS_beta(t_s, i_ex) = 0;
 end
 
 %DRO
-if V_e >= V_DRO(t_s, i_ex)
+if V_e_true >= V_DRO(t_s, i_ex)
     disap_DRO(t_s, i_ex) = 1;
 else
     disap_DRO(t_s, i_ex) = 0;
 end
+
+%DR 
+if V_e_true >= R_DR(t_s, i_ex)
+    disap_DR(t_s, i_ex) = 1;
+else
+    disap_DR(t_s, i_ex) = 0;
+end
+
+
+
+
+
+
+
+
 
 
 end
@@ -340,7 +749,15 @@ end
 DataMatrix = V_DRO';
 DataMatrix = V_IPS';
 DataMatrix = V_IPS_beta';
+DataMatrix = R_DR';
 save('off_policy_experiment_data')
+
+
+
+
+
+
+
 
 
 
@@ -351,25 +768,51 @@ for ts=1:length(T_vec)
     di_IPS(ts) = sum(disap_IPS(ts,:))/N_ex;
     di_IPS_beta(ts) = sum(disap_IPS_beta(ts,:))/N_ex;
     di_DRO(ts) = sum(disap_DRO(ts,:))/N_ex;
+    di_DR(ts) = sum(disap_DR(ts,:))/N_ex;
 end
 
 figure
-title('disappointment')
+title(['r=',num2str(param.r)])
 plot(T_vec,di_IPS,'blue')
 hold on
 plot(T_vec,di_IPS_beta,'cyan')
-plot(T_vec,di_DRO,'red')
-legend('IPS','IPS_beta','DRO')
+plot(T_vec,di_DRO,'green')
+plot(T_vec,di_DR,'magenta')
+legend('IWERM','capped IWERM','MDI-DRO','doubly robust method')
+xlabel('N')
+ylabel('disappointment')
+
 
 
 figure
 hold on
-title('Value')
+title(['r=',num2str(param.r)])
 for i=1:N_ex
     plot(T_vec,V_IPS(:,i),'b x')
     plot(T_vec,V_IPS_beta(:,i),'c x')
-    plot(T_vec,V_DRO(:,i),'r x')
-    plot(T_vec,ones(length(T_vec),1)*V_e,'g')
+    plot(T_vec,V_DRO(:,i),'g x')
+    plot(T_vec,ones(length(T_vec),1)*V_e_true,'r')
 end
-legend('IPS','beta-IPS','DRO','true cost')
+legend('IWERM','capped IWERM','MDI-DRO','ground truth')
+xlabel('N')
+ylabel('R(\theta^*_N,P_{te})')
+
+
+
+
+figure
+hold on
+title(['r=',num2str(param.r)])
+for i=1:N_ex
+    plot(T_vec,V_DRO(:,i),'g x')
+    plot(T_vec,ones(length(T_vec),1)*V_e_true,'r')
+    plot(T_vec,R_DR(:,i),'m o')
+end
+legend('MDI-DRO','ground truth','doubly robust method')
+xlabel('N')
+ylabel('R(\theta^*_N,P_{te})')
+
+
+
+save('OPE_sim')
 
